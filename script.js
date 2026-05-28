b489472493b1f9f59ab1f0e9091d2c7d74feab68
Christian Luiz <imnotchx02@gmail.com>
2026-05-28 17:05:42 -0400

    description change

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ function trocademodo(){
     const img = document.querySelector('#profile img')
      if(html.classList.contains('light')) {
        img.setAttribute('src', './FOTOS/icon 1.png')
+       img.setAttribute('alt', 'Rapaz com tatuagem e brincos sentado em uma cadeira bebendo agua com gás')
+
     } else {
        img.setAttribute('src', './FOTOS/icon 2.png')
+       img.setAttribute('alt', 'Rapaz com um casaco azul, com capuz tampando o rosto com a mão')
     }
 }
